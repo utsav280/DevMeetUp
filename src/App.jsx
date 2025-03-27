620eef7d22dd040dcd8f5a090d163ab61074c252
utsav280 <utsav28feb@gmail.com>
2025-03-27 14:41:47 +0530

    add routing and layout components; integrate daisyUI for styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "../src/components/Login";
+
+import Body from "../src/components/Body";
+
 function App() {
   return (
     <div>
-      <p className="font-bold text-2xl text-center">Hello Vite</p>
+      <BrowserRouter basename="/">
+        <Routes>
+          <Route path="/" element={<Body />}>
+            <Route path="/login" element={<Login />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
